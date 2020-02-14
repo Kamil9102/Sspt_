8fbe0919811ada312ce0e08873084f535a6949b1
Kamil9102 <58091828+Kamil9102@users.noreply.github.com>
2020-02-14 23:07:07 +0300

    Update_v

--- aCSV.py
+++ aCSV.py
@@ -1,18 +1,22 @@
 import csv
+from datetime import datetime
 
-type_of_activity = { 
-    'спорт': 2,
-    'прогулка': 1,
-    'дорога до работы': 1.1,
-    'домашние дела': 1.3,  
-    'приготовление еды': 1.3,
-    'кодинг': 2,
-    'пробежка': 1.3,
-}
+dt_now = datetime.now()
+time = datetime.strftime(dt_now, '%d/%m/%y %H:%M:%S.%f')
+
+type_of_activity = [
+    {'activity': 'спорт', 'time': '2:00:00'},
+    {'activity': 'прогулка', 'time': '1:00:00'},
+    {'activity': 'дорога до работы', 'time': '1:10:00'},
+    {'activity': 'домашние дела', 'time': '1:30:00'},  
+    {'activity': 'приготовление еды', 'time': '1:30:00'},
+    {'activity': 'кодинг', 'time': '2:00:00'},
+    {'activity': 'пробежка', 'time': '1:30:00'},
+]
 
 with open('project.csv', 'w', encoding='utf-8') as project:
-    fields = ['actyvity', 'time']
+    fields = ['activity', 'time']
     writer = csv.DictWriter(project, fields, delimiter=';')
     writer.writeheader()
-    for actyvity in type_of_activity:
-        writer.writerow(f'(type_of_activity[actyvity])')
+    for activity in type_of_activity:
+        writer.writerow(activity)
